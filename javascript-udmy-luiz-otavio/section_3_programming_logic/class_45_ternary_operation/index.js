e34ffe7303c68fb3a0d10f49a5969a44b8ba3935
Daniel <Daniel.2019Amorim@gmail.com>
2024-06-07 06:41:38 -0300

    Inserting class 46
    Lesson on ternary operation

--- javascript-udmy-luiz-otavio/section_3_programming_logic/class_45_ternary_operation/index.js
+++ javascript-udmy-luiz-otavio/section_3_programming_logic/class_45_ternary_operation/index.js
@@ -1,1 +1,22 @@
-console.log('retomando as aulas...')
+// We use the characters '?' and ':' to creat ternary operation.
+
+let pontuacaoUsuario = 1000;
+
+if (pontuacaoUsuario > 1000) {
+    console.log('Usuário vip');
+} else {
+    console.log('Usuário normal');
+}
+
+// for examplo: (condition) ? 'Value for true' : 'Value for false';
+
+let nivelUsuario = pontuacaoUsuario > 1000 ? 'Usuário vip' : 'Usuário normal';
+
+console.log(nivelUsuario)
+
+// Another examplo:
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preto';
+
+console.log(corPadrao);
