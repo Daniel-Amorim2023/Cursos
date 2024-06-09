ef8faa4bcd0a68c8f010473a3e1549a82aad4e34
Daniel <Daniel.2019Amorim@gmail.com>
2024-06-09 09:23:22 -0300

    Revert "inserindo o curso de javascript"
    This reverts commit 0b658e3a047d3a9db2312469d98c437814cd7900.

--- javascript-udmy-luiz-otavio/section_3_programming_logic/class_47_swtch_case/index.js
+++ javascript-udmy-luiz-otavio/section_3_programming_logic/class_47_swtch_case/index.js
@@ -1,18 +1,82 @@
 const data = new Date();
-const dayOfWeek = data.getDay()
+const daysOfWeek = data.getDay()
 
-if (dayOfWeek === 0) {
+if (daysOfWeek === 0) {
     console.log('Sunday');
-} else if (dayOfWeek === 1) {
+} else if (daysOfWeek === 1) {
     console.log('Monday');
-} else if (dayOfWeek === 2) {
+} else if (daysOfWeek === 2) {
     console.log('Tuesday');
-} else if (dayOfWeek === 3) {
+} else if (daysOfWeek === 3) {
     console.log('wednesday');
-} else if (dayOfWeek === 4) {
+} else if (daysOfWeek === 4) {
     console.log('Thursday');
-} else if (dayOfWeek === 5) {
+} else if (daysOfWeek === 5) {
     console.log('Friday');
-} else if (dayOfWeek === 6) {
+} else if (daysOfWeek === 6) {
     console.log('Saturday');
 };
+
+console.log("Situação 1 \n")
+// simpliflying with switch case.
+
+switch (daysOfWeek) {
+    case 0:
+        console.log('Sunday');
+        break;
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursday');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    case 6:
+        console.log('Saturday');
+        break;
+    default:
+        console.log('#')
+}
+
+console.log("Situação 2 \n")
+
+// reusing with function
+
+function getDayOfWeek (day) { 
+    // let dayOfWeek = day;
+    switch (day) {
+        case 0:
+            console.log('Sunday');
+            break;
+        case 1:
+            console.log('Monday');
+            break;
+        case 2:
+            console.log('Tuesday');
+            break;
+        case 3:
+            console.log('Wednesday');
+            break;
+        case 4:
+            console.log('Thursday');
+            break;
+        case 5:
+            console.log('Friday');
+            break;
+        case 6:
+            console.log('Saturday');
+            break;
+        default:
+            console.log('#')}
+}
+
+getDayOfWeek (daysOfWeek);
+console.log("Situação 3")
