2d1e0f3c139adf730542cbcfcdac967d29ad2acc
Daniel <Daniel.2019Amorim@gmail.com>
2024-06-13 10:33:17 -0300

    Adding lesson 52
    Lesson about repite structure for

---
+++ javascript-udmy-luiz-otavio/section_3_programming_logic/classes/class_52_classic_for/index.js
@@ -0,0 +1,15 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+
+for (let i = 0; i < 5; i++) {
+    console.log(`LInha ${i}`);
+}
+
+let frutas = ['Maçã', 'Uva', 'Jabuticaba', 'Melância', 'Banana'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}, fruta ${frutas[i]}`);
+}
