baf020f4a8edf04eb34c6dbb8aea1feff4fc7aa2
Daniel <Daniel.2019Amorim@gmail.com>
2024-06-14 11:44:56 -0300

    Adding class 55
    Lesson about repite structure for of

---
+++ javascript-udmy-luiz-otavio/section_3_programming_logic/classes/class_55_for_of/index.js
@@ -0,0 +1,19 @@
+let nomeCompleto = 'Daniel da Silva Lemos Amorim';
+
+let nomes = ['Karina', 'Daniel', 'Biel', 'lena'];
+
+// for (let i = 0; i < nomeCompleto.length; i++) {
+//     console.log(nomeCompleto[i]);
+// }
+
+// for (let i in nomeCompleto) {
+//     console.log(nomeCompleto[i]);
+// }
+
+// for (let letra of nomeCompleto) {
+//     console.log(letra);
+// }
+
+for (let nome of nomes) {
+    console.log(nome);
+}
