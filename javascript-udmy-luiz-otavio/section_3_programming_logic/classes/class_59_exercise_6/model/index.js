18195958382a37a0229e9fc22ac3b3a68cc7ee81
Daniel <Daniel.2019Amorim@gmail.com>
2024-06-17 22:10:50 -0300

    Update 1.0 exercise 6

--- javascript-udmy-luiz-otavio/section_3_programming_logic/classes/class_59_exercise_6/model/index.js
+++ javascript-udmy-luiz-otavio/section_3_programming_logic/classes/class_59_exercise_6/model/index.js
@@ -1,7 +1,5 @@
 function main () {
-    const body = document.querySelector("#body");
-    const section = document.getElementById('section');
-    const title = document.querySelector('.title');
+    
     
 }
 
