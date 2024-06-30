3ce30dc3b6b81e1737853a2564c1f51369096518
Daniel <Daniel.2019Amorim@gmail.com>
2024-06-29 22:13:25 -0300

    update class 75

--- javascript-udmy-luiz-otavio/section_4_javascript_function_advanced/classes/class_75_factory_functions/index.js
+++ javascript-udmy-luiz-otavio/section_4_javascript_function_advanced/classes/class_75_factory_functions/index.js
@@ -1,16 +1,35 @@
 (function (){
-    function pessoa (nome, sobrenome) {
+    function pessoa (nome, sobrenome, peso, altura) {
         return {
             nome,
             sobrenome,
+            // fala: function (assunto) {
             fala: function (assunto) {
-                console.log(this.nome + ' ' + assunto)
-                // console.log(nome + ' ' + assunto)
+                console.log(this.nome + ' ' + assunto);
+                // console.log(nome + ' ' + assunto)        
+            },
+            get nomeCompleto () {
+                return this.nome + ' ' + this.sobrenome;
+            },
+            set nomeCompleto (valor) {
+                valor = valor.split(' ');
+                this.nome = valor.shift();
+                this.sobrenome = valor.join(' ');
+
+            },
+            peso,
+            altura, 
+            get imc() {
+                const index = this.peso / (this.altura ** 2);
+                return index.toFixed(2);
             }
         }
     } 
-    const p1 = pessoa('Daniel', 'Amorim')
-    p1.fala('fala bem!')
+    const p1 = pessoa('Daniel', 'Amorim', 80, 1.8)
+    p1.nomeCompleto = 'Karina prado';
+    p1.fala(`seu imc é ${p1.imc}`)
+    console.log(p1.nome, p1.sobrenome);
+    console.log(p1.imc);
+    
 })()
 
-// This command is used refer all object.
