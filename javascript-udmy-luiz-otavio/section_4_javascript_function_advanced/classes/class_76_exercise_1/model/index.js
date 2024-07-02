faf49a78f37049b4f5ac3d5a408f1f89944f108c
Daniel <Daniel.2019Amorim@gmail.com>
2024-07-02 13:30:01 -0300

    Finishing exercise 76
    exercise about calculater

--- javascript-udmy-luiz-otavio/section_4_javascript_function_advanced/classes/class_76_exercise_1/model/index.js
+++ javascript-udmy-luiz-otavio/section_4_javascript_function_advanced/classes/class_76_exercise_1/model/index.js
@@ -1,5 +1,73 @@
-function main () {
-    
+function creteCalculator () {
+    return {
+        display: document.querySelector('.display'),
+
+
+        start() {
+            this.clickButtons();
+            this.squeezeEnter();
+        },
+
+        clearCharacter () {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        clearDisplay () {
+            this.display.value = ''
+        },
+
+        executeAccount () {
+            let account = this.display.value;
+            try {
+                account = eval(account);
+// eval is a command that requeres attention due to malicious code 
+
+                if (!account) {
+                    alert('Invalid account');
+                    return;
+                    };
+                this.display.value = account; 
+            } catch (e) {
+                alert('invalid account!');
+                return;
+            }
+        },
+
+        squeezeEnter () {
+            this.display.addEventListener('keypress', e => {
+                if (e.keyCode === 13) {
+                    this.executeAccount();
+                }
+            })
+        },
+
+        clickButtons () {
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnToDisplay(el.innerText);
+                };
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-dell')) {
+                    this.clearCharacter();
+                }
+
+                if (el.classList.contains('btn-equal')) {
+                    this.executeAccount()
+                }
+            })//,bind(this)
+        },
+
+        btnToDisplay (text) {
+            this.display.value += text;
+        }
+    }
 }
 
-main();
+const calculator = creteCalculator();
+calculator.start()
